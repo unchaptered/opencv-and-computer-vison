f5fa04efcea09a21cf2859dbf86151ca01fefcaf
Minsoek, lee <workstation19961002@gmail.com>
2023-01-24 19:47:59 +0900

    Fix(Automation) : new except list in EXCEPT_SET

--- src/folder.explorer.ts
+++ src/folder.explorer.ts
@@ -2,19 +2,25 @@ import fs from 'fs';
 import path from 'path';
 
 const EXCEPT_SET = new Set([
-    '.git',
-    '.github',
-    '.gitignore',
-    '.DS_Store',
-    '.ipynb_checkpoints',
-    'dist',
-    'images',
-    'node_modules',
-    'package-lock.json',
-    'package.json',
-    'README.md',
-    'src',
-    'tsconfig.json'
+    '.git',                 // For Repositories
+    '.github',              // For Repositories
+    '.gitignore',           // For Repositories
+    '.DS_Store',            // For Repositories
+    '.ipynb_checkpoints',   // For *_**_...
+    'cfg',                  // For 8_Yolo_V3
+    'model',                // For 8_Yolo_V3
+    'videos',               // For 8_Yolo_V3
+    'demo.py',              // For 8_Yolo_V3
+    'yad2k.py',             // For 8_Yolo_V3
+    'LICENSE',              // For 8_Yolo_V3
+    'dist',                 // For Repositories
+    'images',               // For Repositories
+    'node_modules',         // For Repositories
+    'package-lock.json',    // For Repositories
+    'package.json',         // For Repositories
+    'README.md',            // For Repositories
+    'src',                  // For Repositories
+    'tsconfig.json'         // For Repositories
 ]);
 
 export function getFolderList(fileName: string): string[] {
